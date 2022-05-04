f6b9cda8800d10ed926554ad2d6e2388c0860ed7
Mircea-Andrei16 <andreipomacu16@gmail.com>
2022-05-04 23:30:29 +0300

    creation of the intial project

---
+++ BookingFlights/BookingFlights/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace BookingFlights.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
