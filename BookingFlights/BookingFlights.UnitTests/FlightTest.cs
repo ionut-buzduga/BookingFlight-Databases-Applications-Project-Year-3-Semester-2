6f2f5e08a66513c662f9b85acb8ea30f8abeec56
Robert Draghici <robertcristy20@gmail.com>
2022-05-26 00:21:04 +0300

    Implementation of unit tests for flights and tickets

--- BookingFlights/BookingFlights.UnitTests/FlightTest.cs
+++ BookingFlights/BookingFlights.UnitTests/FlightTest.cs
@@ -1,4 +1,9 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using BookingFlights.Abstractions.Repository;
+using BookingFlights.AppLogic.Services;
+using BookingFlights.DataAccess;
+using BookingFlights.DataModel;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,7 +13,50 @@ using System.Threading.Tasks;
 namespace BookingFlights.UnitTests
 {
     [TestClass]
-    public  class FlightTest
+    public class FlightTest
     {
+        //connection string
+        private const string ConnectionString = @"Server=(localdb)\mssqllocaldb;Database=BookingFlightDb;Trusted_Connection=True";
+
+        //function for intialization of the service
+        private FlightService init()
+        {
+            BookingFlightsDbContext dbContext = new BookingFlightsDbContext(new DbContextOptionsBuilder<BookingFlightsDbContext>()
+                .UseSqlServer(ConnectionString)
+                .Options);
+            IRepositoryWrapper repositoryWrapper = new RepositoryWrapper(dbContext);
+            return new FlightService(repositoryWrapper);
+        }
+
+        [TestMethod]
+        public void GetAllFlightsCreated()
+        {
+            //Arange
+            FlightService _flightService = init();
+
+            //Act
+            var flightList = _flightService.GetAllQueryable().ToList();
+
+            //Assert
+            Assert.AreEqual(6, flightList.Count);
+        }
+
+        [TestMethod]
+        public void SearchFlight_Works()
+        {
+            //Arange
+            FlightService _flightService = init();
+            Guid FlightId = Guid.Parse("71b931fb-1c09-4bb4-99ae-a04be89bbaf6");
+            string FlightName = "Z3123";
+            string departureCity = "Craiova";
+            string arrivalCity = "Cluj";
+            DateTime departureDate = new DateTime(2022, 05, 18, 23, 48, 00);
+
+            //Act
+            var searchflight = _flightService.SearchFlight(departureCity, arrivalCity, departureDate);
+
+            //Assert
+            Assert.IsNotNull(searchflight);
+        }
     }
 }
--- BookingFlights/BookingFlights.UnitTests/TicketTest.cs
+++ BookingFlights/BookingFlights.UnitTests/TicketTest.cs
@@ -1,14 +1,121 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using BookingFlights.Abstractions.Repository;
+using BookingFlights.AppLogic.Services;
+using BookingFlights.DataAccess;
+using BookingFlights.DataModel;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NUnit.Framework;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Moq;
+using Assert = NUnit.Framework.Assert;
 
 namespace BookingFlights.UnitTests
 {
     [TestClass]
     public class TicketTest
     {
+        //connection string
+        private const string ConnectionString = @"Server=(localdb)\mssqllocaldb;Database=BookingFlightDb;Trusted_Connection=True";
+
+        //function for intialization of the service
+        private TicketService init()
+        {
+            BookingFlightsDbContext dbContext = new BookingFlightsDbContext(new DbContextOptionsBuilder<BookingFlightsDbContext>()
+                .UseSqlServer(ConnectionString)
+                .Options);
+            IRepositoryWrapper repositoryWrapper = new RepositoryWrapper(dbContext);
+            return new TicketService(repositoryWrapper);
+        }
+
+        [TestMethod]
+        public void GetAllTicketsCreated()
+        {
+            //Arange
+            TicketService _ticketService = init();
+
+            //create 3 bookings
+            Ticket ticket1 = new Ticket()
+            {
+                Type = "Economy",
+                FlightId = Guid.NewGuid(),
+                Price = 80
+            };
+            Ticket ticket2 = new Ticket()
+            {
+                Type = "Business",
+                FlightId = Guid.NewGuid(),
+                Price = 100
+            };
+            Ticket ticket3 = new Ticket()
+            {
+                Type = "All-inclusive",
+                FlightId = Guid.NewGuid(),
+                Price = 150
+            };
+
+            //Act
+            var ticketList = _ticketService.GetAllQueryable().ToList();
+
+            //Assert
+            Assert.AreEqual(3, ticketList.Count + 1);
+        }
+
+        //[TestMethod]
+        //public void TicketForFlight_Works()
+        //{
+        //    //Arrange
+        //    TicketService _ticketService = init();
+        //    Ticket ticket1 = new Ticket()
+        //    {
+        //        Type = "Economy",
+        //        FlightId = Guid.NewGuid(),
+        //        Price = 80
+        //    };
+
+        //    //Act
+        //    Ticket specificTicket = _ticketService.TicketForFLight(ticket1);
+
+        //    //Assert
+        //    Assert.
+        //}
+
+        //[TestMethod]
+        //public void FindSpecificFlight()
+        //{
+        //    //Arrange
+        //    TicketService _ticketService = init();
+        //    Guid FlightId = Guid.Parse("2f5f24e1-5687-4dab-3ec8-08da3968162b");
+        //    Ticket ticket = new Ticket()
+        //    {
+        //        FlightId = Guid.Parse("2f5f24e1-5687-4dab-3ec8-08da3968162b"),
+        //        Type = "economy",
+        //        Price = 80
+        //    };
+        //    //Act
+        //    Ticket findflight = _ticketService.findSpecificFlight(FlightId);
+
+        //    //Assert
+        //    Assert.AreEqual(findflight.FlightId, ticket.FlightId);
+        //}
+
+        //[TestMethod]
+        //public void DeleteFromEntity_Works()
+        //{
+        //    //Arrange
+        //    TicketService _ticketService = init();
+        //    Guid TicketId = Guid.Parse("0b109973-1f2c-4e1f-e569-08da399c3869");
+        //    Ticket ticket = new Ticket()
+        //    {
+        //        Id = Guid.Parse("0b109973-1f2c-4e1f-e569-08da399c3869"),
+
+        //    };
+
+        //    var findticket = _ticketService.DeleteFromEntity();
+        //    Assert.IsNotNull(findticket);
+        //}
     }
 }
